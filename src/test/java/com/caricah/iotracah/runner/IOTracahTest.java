39039179726e819d62f367de74dd96cf34282711
Peter J. Bwire <bwire@caricah.com>
2015-09-27 15:43:53 +0300

    merge with the committed error

---
+++ src/test/java/com/caricah/iotracah/runner/IOTracahTest.java
@@ -0,0 +1,51 @@
+/*
+ *
+ * Copyright (c) 2015 Caricah <info@caricah.com>.
+ *
+ * Caricah licenses this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ *  of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under
+ *  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ *  OF ANY  KIND, either express or implied.  See the License for the specific language
+ *  governing permissions and limitations under the License.
+ *
+ *
+ *
+ *
+ */
+
+package com.caricah.iotracah.runner;
+
+import com.caricah.iotracah.runner.impl.DefaultRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.Assert.*;
+/**
+ * @author <a href="mailto:bwire@caricah.com"> Peter Bwire </a>
+ * @version 1.0 8/9/15
+ */
+
+public class IOTracahTest {
+
+    @Test
+    public void testMain() throws Exception {
+
+        IOTracah runner = Mockito.spy(new IOTracah());
+
+        //TODO: test this method when time is available.
+
+
+    }
+}
